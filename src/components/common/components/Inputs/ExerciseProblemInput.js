fed5c6f8c95bd857e1a6d92583eedcced59236f3
Lorenzo Gimenez Luzny <lgimenez@fi.uba.ar>
2024-07-15 21:55:48 -0300

    Add custom tooltip based on exerciseType

--- src/components/common/components/Inputs/ExerciseProblemInput.js
+++ src/components/common/components/Inputs/ExerciseProblemInput.js
@@ -8,12 +8,28 @@ import MathTextBox from "../../math/MathTextBox";
 import MathTable from "../../../scenes/exercises/MathTable";
 import wandIcon from "../../../../images/wand-icon.svg";
 import { cleanExpression } from "../../../../utils/latexUtils";
+import {ExerciseType} from "../../../../utils/exerciseType";
 
 export default function ExerciseProblemInput(props) {
 	const { content, onChangeExercise, onClickSymbol, customTitle, mathBoxRef, exerciseType, generateProblem} = props;
 
 	const buildTitleCopy = (exerciseType) => {
-		return "la función del enunciado"
+		switch (exerciseType) {
+			case ExerciseType.FACTORISABLE:
+				return "el polinomio a factorear"
+			case ExerciseType.DOMAIN:
+				return "la función a la que se le calculará el dominio"
+			case ExerciseType.INEQUALITY:
+				return "la inecuación a resolver"
+			case ExerciseType.IMAGE:
+				return "la función a la que se le calculará la imagen"
+			case ExerciseType.EXPONENTIAL:
+				return "la ecuación exponencial a resolver"
+			case ExerciseType.INTERSECTION:
+				return "las dos funciones igualadas"
+			default:
+				return "la función del enunciado"
+		}
 	}
 
 	const overrideMathBoxContext = (content) => {
@@ -28,7 +44,7 @@ export default function ExerciseProblemInput(props) {
 		<div className={createExerciseStyles.exerciseContainer}>
 			<div className={createExerciseStyles.writeExerciseTitle}>
 				<Typography color="textSecondary" variant="body2"> {customTitle || "Escriba el ejercicio"} </Typography>
-				<BootstrapTooltip title={"Escribi " + buildTitleCopy(exerciseType) + " o usá la varita mágica para autogenerarla"}
+				<BootstrapTooltip title={"Escribi " + buildTitleCopy(exerciseType) + " o usá la varita mágica para auto-generar un enunciado"}
 													placement="top-start">
 					<InfoIcon id="info-icon" fontSize="small" className={createExerciseStyles.icon}/>
 				</BootstrapTooltip>
---
+++ src/utils/exerciseType.js
@@ -0,0 +1,11 @@
+export const ExerciseType = {
+	DERIVATIVE: 'derivative',
+	INTEGRAL: 'integral',
+	FACTORISABLE: 'factorisable',
+	DOMAIN: 'domain',
+	INEQUALITY: 'inequality',
+	IMAGE: 'image',
+	TRIGONOMETRY: 'trigonometry',
+	EXPONENTIAL: 'exponential',
+	INTERSECTION: 'intersection',
+}
